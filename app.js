84b2c148374bb949f7137aa584cc603fcf1fbef1
StephanKluge <114141614+StephanKluge@users.noreply.github.com>
2024-08-17 20:39:13 +0200

    einfache funktion zum verdeutlichen des ablaufs einer funktion

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ function fovoriteAnimal(myAnimal) { //declare a functio functionname(parameter){
 const chooseAnimal = prompt("Choose your favorite Animal:", ""); //create a prompt where we should say what our favoriote animal is
 const animal = chooseAnimal;//safe the prompt
 console.log(fovoriteAnimal(animal));//now we say JS we put the value, that safed in "animal" to our function to MyAnimal
+
+//Ganz einfache Funktion
+
+function myFunction(){ //start einer funktion mit eigenen funktionsnamen (frei wählbar)
+    alert("hello");//code innerhalb der funktion
+}
+myFunction();//aufruf der funktion
